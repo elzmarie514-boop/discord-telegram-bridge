b26bf79ccfb717f23b94abdca40d251e70f4e1a6
elzmarie514-boop <elzmarie514@gmail.com>
2026-03-05 20:27:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import discord
+import requests
+import os
 
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.channel.id == CHANNEL_ID and not message.author.bot:
+        text = f"{message.author}: {message.content}"
+
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        requests.post(url, data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": text
+        })
+
+client.run(DISCORD_TOKEN)
