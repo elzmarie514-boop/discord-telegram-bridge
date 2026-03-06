e42b892addfd2ee54a108a436249defafa0c2a7e
elzmarie514-boop <elzmarie514@gmail.com>
2026-03-06 01:23:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import requests
 import os
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,20 +20,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.author.bot:
-        return
-
-    print("Message:", message.content)
-    print("Channel:", message.channel.id)
-
     if message.channel.id == CHANNEL_ID:
 
+        clean_text = message.content.replace(":", ":\n")
+
         text = f"""
-📨 Discord Message
+📢 Discord Message
 
-👤 {message.author}
+👤 {message.author.name}
 
-💬 {message.content}
+{clean_text}
 """
 
         url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
@@ -41,7 +38,5 @@ async def on_message(message):
             "chat_id": TELEGRAM_CHAT_ID,
             "text": text
         })
-
-        print("Sent to Telegram")
 
 client.run(DISCORD_TOKEN)
